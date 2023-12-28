bf32aefce234891e16757caba7cd9e96ed6c2941
shrall <shrallvierdo@gmail.com>
2023-12-28 16:39:33 +0700

    Initial commit

---
+++ src/main/java/com/shrall/tutorialspringsecurity/TutorialSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.shrall.tutorialspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringSecurityApplication.class, args);
+	}
+
+}
