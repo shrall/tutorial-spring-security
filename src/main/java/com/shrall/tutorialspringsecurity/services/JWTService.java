7fbdbc6e2559bbd0352f67ae6e8f78efff52fd50
shrall <shrallvierdo@gmail.com>
2023-12-29 14:48:46 +0700

    feat: create JWT service

---
+++ src/main/java/com/shrall/tutorialspringsecurity/services/JWTService.java
@@ -0,0 +1,17 @@
+package com.shrall.tutorialspringsecurity.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JWTService {
+
+    String generateToken(UserDetails userDetails);
+
+    String extractUsername(String token);
+
+    // Date extractExpiration(String token);
+
+    // boolean isTokenExpired(String token);
+
+    // boolean validateToken(String token, UserDetails userDetails);
+
+}
---
+++ src/main/java/com/shrall/tutorialspringsecurity/services/impl/JWTServiceImpl.java
@@ -0,0 +1,64 @@
+package com.shrall.tutorialspringsecurity.services.impl;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+public class JWTServiceImpl {
+    // NOTE - bad practice to store secret key application.properties
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    private String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // public Date extractExpiration(String token) {
+    // return extractClaim(token, Claims::getExpiration);
+    // }
+
+    // public boolean isTokenExpired(String token) {
+    // return extractExpiration(token).before(new Date());
+    // }
+
+    // public boolean validateToken(String token, UserDetails userDetails) {
+    // return extractUsername(token).equals(userDetails.getUsername()) &&
+    // !isTokenExpired(token);
+    // }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Key getSignInKey() {
+        byte[] key = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(key);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
