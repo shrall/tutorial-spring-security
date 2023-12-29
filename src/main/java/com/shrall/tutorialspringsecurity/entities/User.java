c536bd3983fe68362e6cf47bd734bae9b235d18f
shrall <shrallvierdo@gmail.com>
2023-12-29 13:28:21 +0700

    feat: implement UserDetails on User entity

--- src/main/java/com/shrall/tutorialspringsecurity/entities/User.java
+++ src/main/java/com/shrall/tutorialspringsecurity/entities/User.java
@@ -1,5 +1,12 @@
 package com.shrall.tutorialspringsecurity.entities;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -9,7 +16,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
@@ -20,4 +27,34 @@ public class User {
     private String email;
     private String password;
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // TODO: implement this
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // TODO: implement this
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // TODO: implement this
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // TODO: implement this
+    }
 }
