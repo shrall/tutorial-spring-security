fee1b9347766e9933b57576def240412e6e847e9
shrall <shrallvierdo@gmail.com>
2024-01-03 13:01:47 +0700

    feat: add security configuration

---
+++ src/main/java/com/shrall/tutorialspringsecurity/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.shrall.tutorialspringsecurity.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+}
